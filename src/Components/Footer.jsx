2dbb5d142a989a775c71bf6ed0f9e3c3a54f2d1e
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-11 13:33:16 -0300

    feat: add login component, configurando rotas header e criando rota login

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import styles from './Footer.module.css';
 
-export default function Footer() {
+const Footer = () => {
   return <div>Footer</div>;
-}
+};
+
+export default Footer;
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
-function Header() {
-  return <div>Header</div>;
-}
+const Header = () => {
+  return (
+    <div>
+      <div className={styles.header}>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/login">Login / Criar</Link>
+        </nav>
+      </div>
+    </div>
+  );
+};
 
 export default Header;
