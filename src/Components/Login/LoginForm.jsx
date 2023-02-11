2480a878649f902c5898d80ee2384ed41e56d570
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-11 14:28:37 -0300

    feat: add formulario de login
    pegando os valores do usuario e armazenando em um estado

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const LoginForm = () => {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
   return (
     <section>
       <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={username}
+          onChange={({ target }) => setUsername(target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={({ target }) => setPassword(target.value)}
+        />
+        <button>Entrar</button>
+      </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
   );
