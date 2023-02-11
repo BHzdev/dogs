ca22172acf93ffca06b2bdb5b0395a0492187e30
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-11 14:34:36 -0300

    feat: add função handleSubmit
    faz um fetch com o metodo post na api rest dogs

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -5,6 +5,22 @@ const LoginForm = () => {
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    })
+      .then((response) => {
+        console.log(response);
+        return response.json();
+      })
+      .then((json) => console.log(json));
+  }
+
   return (
     <section>
       <h1>Login</h1>
