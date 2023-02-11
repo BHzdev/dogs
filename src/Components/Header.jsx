330324ec1cd50ad315fbdea78f4258b48a4e91aa
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-11 14:13:58 -0300

    feat: add estilo header, header finalizado

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,14 +6,16 @@ import { ReactComponent as Dogs } from '../Assets/dogs.svg';
 const Header = () => {
   return (
     <div>
-      <div className={styles.header}>
-        <nav className="container">
-          <Link to="/" aria-label="Dogs - Home">
+      <header className={styles.header}>
+        <nav className={`${styles.nav} container`}>
+          <Link className={styles.logo} to="/" aria-label="Dogs - Home">
             <Dogs />
           </Link>
-          <Link to="/login">Login / Criar</Link>
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
         </nav>
-      </div>
+      </header>
     </div>
   );
 };
