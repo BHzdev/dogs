fbbfa0496cbd4824bb037825309cab6d170c91c4
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-12 12:53:17 -0300

    feat: add forms components, button e input

---
+++ src/Components/Forms/Button.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Buttom.module.css';
+
+const Button = () => {
+  return <button className={styles.button}>Button</button>;
+};
+
+export default Button;
