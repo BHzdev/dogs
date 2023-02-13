f4d1a9144182c16e1f2c5872b15d5bf364dae8fc
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-13 10:15:15 -0300

    importando endpoint TOKEN_POST no loginform
    atribuindo url e options em uma constante para facilitar o uso do fetch

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { TOKEN_POST } from '../../api';
 import useForm from '../../Hooks/useForm';
 import Button from '../Forms/Button';
 import Input from '../Forms/Input';
@@ -12,13 +13,12 @@ const LoginForm = () => {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(),
-      })
+      const { url, options } = TOKEN_POST({
+        username: username.value,
+        password: password.value,
+      });
+
+      fetch(url, options)
         .then((response) => {
           console.log(response);
           return response.json();
