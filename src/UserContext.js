41ad711b64a39f5281ccbc9629a7fac973ff76cf
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-14 15:58:16 -0300

    definindo estados dentro de userContext

--- src/UserContext.js
+++ src/UserContext.js
@@ -3,5 +3,14 @@ import React from 'react';
 export const UserContext = React.createContext();
 
 export const UserStorage = ({ children }) => {
-  return <UserContext.Provider>{children}</UserContext.Provider>;
+  const [data, setData] = React.useState(null);
+  const [login, setLogin] = React.useState(null);
+  const [loading, setLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
+
+  return (
+    <UserContext.Provider value={{ usuario: 'André' }}>
+      {children}
+    </UserContext.Provider>
+  );
 };
