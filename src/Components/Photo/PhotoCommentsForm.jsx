d73ec77e43b5b63fee4354c9bf733dad1a585311
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-21 13:57:52 -0300

    add PhotoComments estilo, iniciando estilo PhotoCommentsForm

--- src/Components/Photo/PhotoCommentsForm.jsx
+++ src/Components/Photo/PhotoCommentsForm.jsx
@@ -3,6 +3,7 @@ import { COMMENT_POST } from '../../Api';
 import { ReactComponent as Enviar } from '../../Assets/enviar.svg';
 import useFetch from '../../Hooks/useFetch';
 import Error from '../Helper/Error';
+import styles from './PhotoCommentsForm.module.css';
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const [comment, setComment] = React.useState('');
@@ -13,6 +14,7 @@ const PhotoCommentsForm = ({ id, setComments }) => {
     const token = window.localStorage.getItem('token');
     const { url, options } = COMMENT_POST(id, { comment }, token);
     const { response, json } = await request(url, options);
+
     if (response.ok) {
       setComment('');
       setComments((comments) => [...comments, json]);
@@ -20,15 +22,16 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id="comment"
         name="comment"
         placeholder="Comente..."
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error} />
