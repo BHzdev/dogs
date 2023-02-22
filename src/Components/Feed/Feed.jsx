4fcd2b4b25c375dce9fe8cdc3162d087036e8e2e
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-22 01:41:55 -0300

    fazendo validações para controlar as requisições

--- src/Components/Feed/Feed.jsx
+++ src/Components/Feed/Feed.jsx
@@ -5,19 +5,34 @@ import FeedPhotos from './FeedPhotos';
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = React.useState(null);
   const [pages, setPages] = React.useState([1]);
+  const [infinite, setInfinite] = React.useState(true);
 
   React.useEffect(() => {
+    let wait = false;
+
     function infiniteScroll(event) {
-      setPages((pages) => [...pages, pages.length + 1]);
-      console.log(event);
+      if (infinite) {
+        const scroll = window.scrollY;
+        const height = document.body.offsetHeight - window.innerHeight;
+
+        if (scroll > height * 0.75 && !wait) {
+          setPages((pages) => [...pages, pages.length + 1]);
+          wait = true;
+          setTimeout(() => {
+            wait = false;
+          }, 500);
+        }
+      }
     }
+
     window.addEventListener('wheel', infiniteScroll);
     window.addEventListener('scroll', infiniteScroll);
     return () => {
       window.removeEventListener('wheel', infiniteScroll);
       window.removeEventListener('scroll', infiniteScroll);
     };
-  }, []);
+  }, [infinite]);
+
   return (
     <div>
       {modalPhoto && (
@@ -29,6 +44,7 @@ const Feed = ({ user }) => {
           user={user}
           page={page}
           setModalPhoto={setModalPhoto}
+          setInfinite={setInfinite}
         />
       ))}
     </div>
