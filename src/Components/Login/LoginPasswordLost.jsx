b6d854ef78d3cacb829bdcb897ea65b6c02cdb0b
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-23 13:12:05 -0300

    feat: add pagina de loginpasswordlost(esqueceu a senha)
    deixando o formulario reativo com o hook useForm

--- src/Components/Login/LoginPasswordLost.jsx
+++ src/Components/Login/LoginPasswordLost.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import useFetch from '../../Hooks/useFetch';
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
 
 const LoginPasswordLost = () => {
-  return <div>LoginPasswordLost</div>;
+  const email = useForm();
+  const { data, loading, error, request } = useFetch();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <section>
+      <h1 className="title">Perdeu a senha?</h1>
+      <form onSubmit={handleSubmit}>
+        <Input label="Email / Usuário" type="text" name="email" {...email} />
+        <Button>Enviar Email</Button>
+      </form>
+    </section>
+  );
 };
 
 export default LoginPasswordLost;
