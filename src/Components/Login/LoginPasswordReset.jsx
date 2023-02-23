3abbe1325ad9a9157ffd69cd66da34d2c37fbfc6
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-23 13:31:20 -0300

    começando passwordReset, pegando as informações do usuario via url

--- src/Components/Login/LoginPasswordReset.jsx
+++ src/Components/Login/LoginPasswordReset.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import useForm from '../../Hooks/useForm';
+import Button from '../Forms/Button';
+import Input from '../Forms/Input';
 
 const LoginPasswordReset = () => {
-  return <div>LoginPasswordReset</div>;
+  const { login, setLogin } = React.useState('');
+  const { key, setKey } = React.useState('');
+  const password = useForm();
+
+  React.useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const login = params.get('login');
+    const key = params.get('key');
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  return (
+    <div>
+      <p>{key}</p>
+      <p>{login}</p>
+      <form onSubmit={handleSubmit}>
+        <Input label="Nova Senha" type="password" name="password" />
+        <Button>Resetar</Button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPasswordReset;
