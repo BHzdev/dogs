f287e3faf0144f78ed3e4b17f16df21b295fb72c
Bruno Henrique <bruno_tcc_tc@hotmail.com>
2023-02-24 13:22:29 -0300

    importando graficos do victory, add graphData retorna um objeto com as informações para os graficos

--- src/Components/User/UserStatsGraphs.jsx
+++ src/Components/User/UserStatsGraphs.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import styles from './UserStatsGraphs.module.css';
+import { VictoryPie, VictoryChart, VictoryBar } from 'victory';
 
 const UserStatsGraphs = ({ data }) => {
-  const [graph, setGraph] = React.useState([]);
+  const [graphData, setGraph] = React.useState([]);
   const [total, setTotal] = React.useState(0);
 
   React.useEffect(() => {
+    const graphData = data.map((item) => {
+      return {
+        x: item.title,
+        y: Number(item.acessos),
+      };
+    });
+
     setTotal(
       data.map(({ acessos }) => Number(acessos)).reduce((a, b) => a + b),
     );
+    setGraph(graphData);
   }, [data]);
 
   return (
