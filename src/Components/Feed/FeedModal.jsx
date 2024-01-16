17e492c6bc041badbafce700867fdb7803a75e2c
BHzdev <bruno_tcc_tc@hotmail.com>
2024-01-16 12:23:15 -0300

    feat: implementing redux in the ui(modal)

--- src/Components/Feed/FeedModal.jsx
+++ src/Components/Feed/FeedModal.jsx
@@ -1,27 +1,25 @@
 import React from "react";
 import styles from "./FeedModal.module.css";
-import useFetch from "../../Hooks/useFetch";
 import Error from "../Helper/Error";
 import Loading from "../Helper/Loading";
-import { PHOTO_GET } from "../../Api";
 import PhotoContent from "../Photo/PhotoContent";
 import { useDispatch, useSelector } from "react-redux";
-import { fetchPhoto } from "../../store/photo";
+import { closeModal } from "../../store/ui";
 
-const FeedModal = ({ photo, setModalPhoto }) => {
+const FeedModal = () => {
+  const { modal } = useSelector((state) => state.ui);
   const { loading, error, data } = useSelector((state) => state.photo);
   const dispatch = useDispatch();
 
-  React.useEffect(() => {
-    dispatch(fetchPhoto(photo.id));
-  }, [dispatch, photo.id]);
-
-  function handleOutsideClick({ target, currentTarget }) {
-    if (target === currentTarget) {
-      setModalPhoto(null);
-    }
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) dispatch(closeModal());
   }
 
+  React.useEffect(() => {
+    dispatch(closeModal());
+  }, [dispatch]);
+
+  if (!modal) return null;
   return (
     <div className={styles.modal} onClick={handleOutsideClick}>
       {error && <Error error={error} />}
